ce68fcc9631b7fbf87f53ea62817b38a3edb76ed
yanturck <56774750+yanturck@users.noreply.github.com>
2020-01-27 15:53:23 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -10,7 +10,7 @@ function minuscula(){
   document.getElementById("email").value = letra;
 }
 
-function pontCPF(){
+function mascaraCPF(){
 	var aux = document.getElementById("cpf").value;
 	if(aux.length == 3){
 		aux += ".";
@@ -21,10 +21,12 @@ function pontCPF(){
 	}else if(aux.length == 11){
 		aux += "-";
 		document.getElementById("cpf").value = aux;
+	}else if(aux.length == 14){
+		document.getElementById("email").focus();
 	}
 }
 
-function pontFone(){
+function mascaraFone(){
 	var aux = document.getElementById("fone").value;
 	if(aux == ""){
 		aux += "(";
@@ -35,14 +37,18 @@ function pontFone(){
 	}else if(aux.length == 10){
 		aux += "-";
 		document.getElementById("fone").value = aux;
+	}else if(aux.length == 15){
+		document.getElementById("cep").focus();
 	}
 }
 
-function pontCEP(){
+function mascaraCEP(){
 	var aux = document.getElementById("cep").value;
 	if(aux.length == 5){
 		aux += "-";
 		document.getElementById("cep").value = aux;
+	}else if(aux.length == 9){
+		document.getElementById("endereco").focus();
 	}
 }
 
@@ -55,46 +61,6 @@ function campVazio(){
 			alert("Preencha todos os campos OBRIGATORIOS!");
 			return false;
 	return true;
-}
-
-function validarCPF(cpf) {	
-	cpf = cpf.replace(/[^\d]+/g,'');	
-	if(cpf == '') return false;	
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-			alert("CPF Inválido!");
-			return false;		
-	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))
-			alert("CPF Inválido!");		
-			return false;		
-	// Valida 2o digito	
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-		alert("CPF Inválido!");
-		return false;		
-	return true;   
 }
 
 function salvar(){
